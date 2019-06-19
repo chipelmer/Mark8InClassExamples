1cfb2e4fe10cea081bc38f0598e06db873dff46b
Chip <chip.elmer@gmail.com>
2019-06-19 12:22:22 -0500

    Add project files.

---
+++ Mark8InClassExamples/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Mark8InClassExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Creating variables with different types
+            string firstName = "Chip";
+            char firstLetterOfFirstName = 'C';
+            int favoriteNumber = 2;
+            double pi = 3.14;
+            bool isMarried = true;
+
+            // an if-statement that checks if the value of `favoriteNumber` is greather than 0
+            if (favoriteNumber > 0)
+            {
+                // this will only run if the conditional was true (favoriteNumber > 0)
+                Console.WriteLine("You have a positive outlook!");
+            }
+        }
+    }
+}
