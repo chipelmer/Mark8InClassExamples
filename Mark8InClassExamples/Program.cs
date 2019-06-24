efd3525c3636a489844e652fff418c54296aa832
Chip <chip.elmer@gmail.com>
2019-06-24 13:55:12 -0500

    Example program to add as many numbers as the user wants

--- Mark8InClassExamples/Program.cs
+++ Mark8InClassExamples/Program.cs
@@ -4,33 +4,28 @@ namespace Mark8InClassExamples
 {
     class Program
     {
-        static int AddNumbersFrom1ToN(int n)
-        {
-            int total = 0;
-            for (int i = 1; i <= n; i++)
-            {
-                total = total + i;
-            }
-            return total;
-        }
-
-        static void AskNumericQuestionThenDisplayTotal(string question)
-        {
-            Console.WriteLine(question);
-            string responseAsAString = Console.ReadLine();
-            int response = int.Parse(responseAsAString);
-
-            // Calling another method that we created
-            int total = AddNumbersFrom1ToN(response);
-
-            Console.WriteLine("If you add all the numbers from 1-" + response + ", the total is " + total);
-        }
-
         static void Main()
         {
-            // Calling a method that then calls another method
-            AskNumericQuestionThenDisplayTotal("What is your favorite number?");
-            AskNumericQuestionThenDisplayTotal("What is your favorite year?");
+            int numberOfAdditions = GetIntegerFromUser("How many numbers would you like to add?");
+
+            int grandTotal = 0;
+            for (int i = 0; i < numberOfAdditions; i++)
+            {
+                // Could do: grandTotal += GetIntegerFromUser("What is number " + (i + 1) + "?");
+                int answer = GetIntegerFromUser("What is number " + (i + 1) + "?");
+                grandTotal += answer; // Same as: grandTotal = grandTotal + answer;
+            }
+
+            Console.WriteLine("The total is " + grandTotal);
+        }
+
+        static int GetIntegerFromUser(string prompt)
+        {
+            Console.WriteLine(prompt);
+            string response = Console.ReadLine();
+            // Could do: return int.Parse(response);
+            int number = int.Parse(response);
+            return number;
         }
     }
 }
