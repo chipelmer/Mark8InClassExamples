898771dac97a89101f586309a0c6bc587ca9022f
Chip <chip.elmer@gmail.com>
2019-06-28 10:01:38 -0500

    Storing user responses into an array

--- Mark8InClassExamples/Program.cs
+++ Mark8InClassExamples/Program.cs
@@ -1,105 +1,40 @@
 ﻿using System;
-using System.Diagnostics;
+using System.Collections.Generic;
 
 namespace Mark8InClassExamples
 {
     class Program
     {
-        static int GetIntegerFromUser(string question)
-        {
-            int integerFromUser;
-            bool success;
-            do
-            {
-                Console.WriteLine(question);
-                string userResponse = Console.ReadLine();
-                success = int.TryParse(userResponse, out integerFromUser);
-            } while (success == false);
-
-            return integerFromUser;
-        }
-
-        static void GiveUserARandomInsult(string[] availableInsults)
-        {
-            Random random = new Random();
-            int insultIndex = random.Next(0, availableInsults.Length);
-            string insult = availableInsults[insultIndex];
-            Console.WriteLine(insult);
-        }
-
-        static void RespondToIncorrectGuess(string[] insultsToBeUsed, string highLowHint)
-        {
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine(highLowHint);
-            GiveUserARandomInsult(insultsToBeUsed);
-        }
-
         static void Main(string[] args)
         {
-            Console.WriteLine("Let's play a guessing game!");
+            Console.WriteLine("How many pets do you have?");
+            string response = Console.ReadLine();
+            int numberOfPets = int.Parse(response);
 
-            int max = GetIntegerFromUser("What max range would you like?");
+            string[] petNames = new string[numberOfPets];
 
-            Random rnd = new Random();
-            int secretNumber = rnd.Next(1, max + 1);
-
-            string[] insults = {
-                "Your father was a hampster and your mother smells of elderberries!",
-                "You are quite the guesser! NOT.",
-                "Maybe you should try a different game. Perhaps jumping down some stairs?",
-                "Try " + (secretNumber + 1) + ". No, really, you should try " + (secretNumber + 1) + "."
-            };
-
-            int score = 0;
-            int guess;
-            bool shouldLaunchVideo = true;
-            do
+            for (int i = 0; i < numberOfPets; i++)
             {
-                Console.ResetColor();
+                Console.WriteLine("What is the name of one of your pets?");
+                string petName = Console.ReadLine();
+                petNames[i] = petName;
+            }
 
-                Console.WriteLine("Your current score is " + score);
-                guess = GetIntegerFromUser("Please guess a number between 1-" + max + ":");
+            //int j = 0;
+            //while (j < numberOfPets)
+            //{
+            //    Console.WriteLine("What is the name of one of your pets?");
+            //    string petName = Console.ReadLine();
+            //    petNames[j] = petName;
+            //    j++;
+            //}
 
-                if (guess > max || guess < 1)
-                {
-                    Console.WriteLine("Is guessing really that hard?");
-
-                    if (shouldLaunchVideo == true)
-                    {
-                        shouldLaunchVideo = false;
-                        Console.WriteLine("Here's a tutorial that's about your competence level:");
-
-                        System.Threading.Thread.Sleep(4000);
-
-                        ProcessStartInfo psi = new ProcessStartInfo();
-                        psi.FileName = "https://www.youtube.com/watch?v=9QDZUrvNI-0";
-                        psi.UseShellExecute = true;
-                        Process.Start(psi);
-                    }
-
-                    score += 10;
-                }
-                else
-                {
-                    if (guess > secretNumber)
-                    {
-                        RespondToIncorrectGuess(insults, "You were too high, loser!");
-                        score += 1;
-                    }
-                    else if (guess < secretNumber)
-                    {
-                        RespondToIncorrectGuess(insults, "You were too low, loser!");
-                        score += 1;
-                    }
-                }
-
-            } while (guess != secretNumber);
-
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine("You finally got it!");
-            Console.WriteLine("Your final score was " + score);
-            Console.WriteLine("Better luck next time!");
-
+            //foreach (string name in petNames)
+            //{
+            //    Console.WriteLine("What is the name of one of your pets?");
+            //    string petName = Console.ReadLine();
+            //    name = petName; // not allowed by C#
+            //}
         }
     }
 }
