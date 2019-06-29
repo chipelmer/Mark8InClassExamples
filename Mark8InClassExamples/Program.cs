fab1fe484b83b03cf4a5bfef2a81920b52670385
Cruz Sanchez <cruzsanchez9209@gmail.com>
2019-06-29 12:56:13 -0500

    Reafctored while loop

--- Mark8InClassExamples/Program.cs
+++ Mark8InClassExamples/Program.cs
@@ -3,25 +3,26 @@ using System.Collections.Generic;
 
 namespace Mark8InClassExamples
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Do you have any pets?");
-            string response = Console.ReadLine();
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Do you have any pets?");
+			string response = Console.ReadLine();
 
-            List<string> petNames = new List<string>();
-            List<string> affirmativeResponses = new List<string>() { "y", "yes", "yeah", "yep", "uh huh" };
+			List<string> petNames = new List<string>();
+			List<string> affirmativeResponses = new List<string>() { "y", "yes", "yeah", "yep", "uh huh" };
 
-            while (affirmativeResponses.Contains(response.ToLower()) == true)
-            {
-                Console.WriteLine("What is the name of one of your pets?");
-                string petName = Console.ReadLine();
-                petNames.Add(petName);
+			//Removed == to true in while loop, is unnecessary since the Contains() returns a bool.
+			while (affirmativeResponses.Contains(response.ToLower()))
+			{
+				Console.WriteLine("What is the name of one of your pets?");
+				string petName = Console.ReadLine();
+				petNames.Add(petName);
 
-                Console.WriteLine("Do you have any more pets?");
-                response = Console.ReadLine();
-            }
-        }
-    }
+				Console.WriteLine("Do you have any more pets?");
+				response = Console.ReadLine();
+			}
+		}
+	}
 }
