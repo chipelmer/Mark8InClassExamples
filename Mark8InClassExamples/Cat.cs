1789589eca82f7b4bf4c0aa5447254c8c46e4c61
Chip <chip.elmer@gmail.com>
2019-07-02 10:39:54 -0500

    Some examples with classes (Dog and Cat)

---
+++ Mark8InClassExamples/Cat.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mark8InClassExamples
+{
+    public class Cat
+    {
+        public string Name { get; set; }
+        public string Color { get; set; }
+
+        public void Meow()
+        {
+            Console.WriteLine("Meeeeoow!! I'm " + Name + "...");
+        }
+    }
+}
--- Mark8InClassExamples/Program.cs
+++ Mark8InClassExamples/Program.cs
@@ -6,23 +6,13 @@ namespace Mark8InClassExamples
     {
         static void Main(string[] args)
         {
-            // This is calling the overload that accepts 2 integers
-            int result1 = Add(2, 3);
+            Dog myExampleDog = new Dog("Spot");
 
-            // This is calling the overload that accepts 3 integers
-            int result2 = Add(12, 20, 9);
-        }
+            // This line isn't needed because the Constructor
+            // is handling assigning the name of the new dog
+            // myExampleDog.Name = "Spot";
 
-
-        // There are 2 overloads for the Add method
-        static int Add(int num1, int num2)
-        {
-            return num1 + num2;
-        }
-
-        static int Add(int num1, int num2, int num3)
-        {
-            return num1 + num2 + num3;
+            myExampleDog.Speak();
         }
     }
 }
