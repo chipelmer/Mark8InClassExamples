7c4f9f537e783826ec4bd8f5cfde2b7dbaeb8ffa
Chip <chip.elmer@gmail.com>
2019-07-02 13:06:31 -0500

    Lemonade stand business analysis

---
+++ Mark8InClassExamples/LemonadeStand.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mark8InClassExamples
+{
+    public class LemonadeStand
+    {
+        public string Name { get; set; }
+        public decimal TableCost { get; set; }
+        public decimal ChairCost { get; set; }
+        public decimal SignCost { get; set; }
+        public decimal LemonsCostPerCup { get; set; }
+        public decimal SugarCostPerCup { get; set; }
+        public decimal PricePerCup { get; set; }
+        public int NumberOfCupsSold { get; set; }
+
+        public decimal GetTotalRevenue()
+        {
+            return NumberOfCupsSold * PricePerCup;
+        }
+
+        public decimal GetTotalExpenses()
+        {
+            decimal fixedCosts = TableCost + ChairCost + SignCost;
+            decimal variableCosts = (LemonsCostPerCup + SugarCostPerCup) * NumberOfCupsSold;
+            return fixedCosts + variableCosts;
+        }
+
+        public decimal GetTotalProfit()
+        {
+            // We could do these 3 lines, or just the 1 line below them:
+            //decimal totalRevenue = GetTotalRevenue();
+            //decimal totalExpenses = GetTotalExpenses();
+            //return totalRevenue - totalExpenses;
+            return GetTotalRevenue() - GetTotalExpenses();
+        }
+    }
+}
--- Mark8InClassExamples/Program.cs
+++ Mark8InClassExamples/Program.cs
@@ -4,15 +4,36 @@ namespace Mark8InClassExamples
 {
     class Program
     {
+        static decimal GetDecimalAnswerFromUser(string question)
+        {
+            Console.WriteLine(question);
+            string response = Console.ReadLine();
+            decimal answer = decimal.Parse(response);
+            return answer;
+        }
+
         static void Main(string[] args)
         {
-            Dog myExampleDog = new Dog("Spot");
+            LemonadeStand stand = new LemonadeStand();
 
-            // This line isn't needed because the Constructor
-            // is handling assigning the name of the new dog
-            // myExampleDog.Name = "Spot";
+            Console.WriteLine("What is the name of this lemonade stand?");
+            stand.Name = Console.ReadLine();
 
-            myExampleDog.Speak();
+            stand.TableCost = GetDecimalAnswerFromUser("How much will your table cost?");
+            stand.ChairCost = GetDecimalAnswerFromUser("How much will your chair cost?");
+            stand.SignCost = GetDecimalAnswerFromUser("How much will your sign cost?");
+            stand.LemonsCostPerCup = GetDecimalAnswerFromUser("How much will the lemons cost per cup?");
+            stand.SugarCostPerCup = GetDecimalAnswerFromUser("How much will the sugar cost per cup?");
+            stand.PricePerCup = GetDecimalAnswerFromUser("What will be the price of a cup?");
+
+            Console.WriteLine("How many cups will you sell?");
+            string numberOfCupsResponse = Console.ReadLine();
+            stand.NumberOfCupsSold = int.Parse(numberOfCupsResponse);
+
+            decimal totalProfit = stand.GetTotalProfit();
+
+            Console.WriteLine("Your profit will be " + totalProfit);
+
         }
     }
 }
