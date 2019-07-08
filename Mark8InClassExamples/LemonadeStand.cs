2e97853bb7c096f9d0103e0e870c29dea84c1a1a
Chip <chip.elmer@gmail.com>
2019-07-08 13:39:06 -0500

    Intro to TDD using xUnit

---
+++ Mark8InClassExamples/LemonadeStand.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mark8InClassExamples
+{
+    public class LemonadeStand
+    {
+        public LemonadeStand(string name)
+        {
+            if (name == null)
+            {
+                Name = "";
+            }
+            else
+            {
+                Name = name;
+            }
+        }
+
+        public string Name { get; set; }
+        public decimal PricePerCup { get; set; }
+        public decimal CostPerCup { get; set; }
+        public int NumberOfCupsSold { get; set; }
+
+        public decimal GetTotalProfit()
+        {
+            return (PricePerCup - CostPerCup) * NumberOfCupsSold;
+        }
+    }
+}
--- Mark8InClassExamples/Program.cs
+++ Mark8InClassExamples/Program.cs
@@ -5,16 +5,7 @@ namespace Mark8InClassExamples
     {
         static void Main(string[] args)
         {
-            Comedian myComedian = new Comedian();
-            Singer mySinger = new Singer();
-
-            DoYourPerformance(myComedian);
-            DoYourPerformance(mySinger);
-        }
-
-        static void DoYourPerformance(IPerform performer)
-        {
-            performer.Perform();
+            
         }
     }
 }
