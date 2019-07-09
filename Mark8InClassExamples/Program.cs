9ec35f0f24327fb1be64f171773cad3d22f6d51c
Chip <chip.elmer@gmail.com>
2019-07-09 11:04:14 -0500

    Extension methods

--- Mark8InClassExamples/Program.cs
+++ Mark8InClassExamples/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using System.Linq; // full of extension methods
 
 namespace Mark8InClassExamples
 {
@@ -6,7 +6,31 @@ namespace Mark8InClassExamples
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, world!");
+            int[] numbers = { 3, 5, 7, 3, 7, 2, 1 };
+            int total = numbers.Sum(); // Sum() is an extension method in the System.Linq namespace
+                                       // Sum() is not a method of the int[] class itself
+
+            int x = 10;
+
+            // The int class does not have a Multiply method
+            // Multiply() is an extension method that we wrote below
+            int y = x.Multiply(99);
+
+            // GetLastCharacter() is an extension method that we wrote below
+            string lastLetter = "Hello".GetLastCharacter();
+        }
+    }
+
+    static class IntegerExtensions
+    {
+        public static int Multiply(this int number, int multiplyBy)
+        {
+            return number * multiplyBy;
+        }
+
+        public static string GetLastCharacter(this string str)
+        {
+            return str[str.Length - 1].ToString();
         }
     }
 }
