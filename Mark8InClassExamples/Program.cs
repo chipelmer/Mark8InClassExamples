00b9698b7010d2ddafbfd7b1db6711c2e87f039d
Chip <chip.elmer@gmail.com>
2019-07-15 11:07:31 -0500

    Example CRUD operations from C#

--- Mark8InClassExamples/Program.cs
+++ Mark8InClassExamples/Program.cs
@@ -1,4 +1,6 @@
-﻿using System.Linq; // full of extension methods
+﻿using System;
+using MySql.Data.MySqlClient;
+using System.Collections.Generic;
 
 namespace Mark8InClassExamples
 {
@@ -6,31 +8,129 @@ namespace Mark8InClassExamples
     {
         static void Main(string[] args)
         {
-            int[] numbers = { 3, 5, 7, 3, 7, 2, 1 };
-            int total = numbers.Sum(); // Sum() is an extension method in the System.Linq namespace
-                                       // Sum() is not a method of the int[] class itself
+            string menuChoice = "";
+            while (menuChoice != "QUIT")
+            {
+                Console.WriteLine("What would you like to do?");
+                Console.WriteLine("INSERT");
+                Console.WriteLine("UPDATE");
+                Console.WriteLine("DELETE");
+                Console.WriteLine("SELECT");
+                Console.WriteLine("QUIT");
 
-            int x = 10;
+                menuChoice = Console.ReadLine();
 
-            // The int class does not have a Multiply method
-            // Multiply() is an extension method that we wrote below
-            int y = x.Multiply(99);
-
-            // GetLastCharacter() is an extension method that we wrote below
-            string lastLetter = "Hello".GetLastCharacter();
+                if (menuChoice.ToUpper() == "INSERT")
+                {
+                    Console.WriteLine("What is the name of the new department?");
+                    string newDepartment = Console.ReadLine();
+                    InsertNewDepartment(newDepartment);
+                }
+                else if (menuChoice.ToUpper() == "UPDATE")
+                {
+                    Console.WriteLine("What deparment name would you like to change?");
+                    string currentName = Console.ReadLine();
+                    Console.WriteLine("What would you like to use as its new name?");
+                    string newName = Console.ReadLine();
+                    UpdateDepartmentName(currentName, newName);
+                }
+                else if (menuChoice.ToUpper() == "DELETE")
+                {
+                    Console.WriteLine("What department name would you like to delete?");
+                    string departmentName = Console.ReadLine();
+                    DeleteDepartment(departmentName);
+                }
+                else if (menuChoice.ToUpper() == "SELECT")
+                {
+                    List<string> departments = GetAllDepartments();
+                    foreach (string department in departments)
+                    {
+                        Console.WriteLine(department);
+                    }
+                }
+            }
         }
-    }
 
-    static class ExtensionMethods
-    {
-        public static int Multiply(this int number, int multiplyBy)
+        static List<string> GetAllDepartments()
         {
-            return number * multiplyBy;
+            MySqlConnection conn = new MySqlConnection();
+            conn.ConnectionString = System.IO.File.ReadAllText("ConnectionString.txt");
+
+            MySqlCommand cmd = conn.CreateCommand();
+
+            cmd.CommandText = "SELECT Name FROM departments;";
+
+            using (conn)
+            {
+                conn.Open();
+
+                List<string> allDepartments = new List<string>();
+
+                MySqlDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read() == true)
+                {
+                    string currentDepartment = reader.GetString("Name");
+                    allDepartments.Add(currentDepartment);
+                }
+
+                return allDepartments;
+            }
         }
 
-        public static string GetLastCharacter(this string str)
+        static void DeleteDepartment(string departmentName)
         {
-            return str[str.Length - 1].ToString();
+            MySqlConnection conn = new MySqlConnection();
+            conn.ConnectionString = System.IO.File.ReadAllText("ConnectionString.txt");
+
+            MySqlCommand cmd = conn.CreateCommand();
+
+            // parameterized query to prevent SQL Injection
+            cmd.CommandText = "DELETE FROM departments WHERE Name = @departmentName;";
+            cmd.Parameters.AddWithValue("departmentName", departmentName);
+
+            using (conn)
+            {
+                conn.Open();
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        static void UpdateDepartmentName(string currentDepartmentName, string newDepartmentName)
+        {
+            MySqlConnection conn = new MySqlConnection();
+            conn.ConnectionString = System.IO.File.ReadAllText("ConnectionString.txt");
+
+            MySqlCommand cmd = conn.CreateCommand();
+
+            // parameterized query to prevent SQL Injection
+            cmd.CommandText = "UPDATE departments SET Name = @newName WHERE Name = @currentName;";
+            cmd.Parameters.AddWithValue("newName", newDepartmentName);
+            cmd.Parameters.AddWithValue("currentName", currentDepartmentName);
+
+            using (conn)
+            {
+                conn.Open();
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        static void InsertNewDepartment(string newDepartmentName)
+        {
+            MySqlConnection conn = new MySqlConnection();
+            conn.ConnectionString = System.IO.File.ReadAllText("ConnectionString.txt");
+
+            MySqlCommand cmd = conn.CreateCommand();
+
+            // parameterized query to prevent SQL Injection
+            cmd.CommandText = "INSERT INTO departments (Name) VALUES (@deptName);";
+            cmd.Parameters.AddWithValue("deptName", newDepartmentName);
+
+            using (conn)
+            {
+                conn.Open();
+                cmd.ExecuteNonQuery();
+            }
         }
     }
 }
