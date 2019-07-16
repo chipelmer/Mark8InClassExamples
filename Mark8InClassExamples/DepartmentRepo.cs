4e61583eb92ed57c4b6a27a340d6f4e5d1790f70
Chip <chip.elmer@gmail.com>
2019-07-16 07:59:25 -0500

    First steps toward refactoring the CRUD operations

---
+++ Mark8InClassExamples/DepartmentRepo.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections.Generic;
+using MySql.Data.MySqlClient;
+
+namespace Mark8InClassExamples
+{
+    class DepartmentRepo
+    {
+        private string connectionString = System.IO.File.ReadAllText("ConnectionString.txt");
+
+        public List<string> GetAllDepartments()
+        {
+            MySqlConnection conn = new MySqlConnection(connectionString);
+
+            MySqlCommand cmd = conn.CreateCommand();
+
+            cmd.CommandText = "SELECT Name FROM departments;";
+
+            using (conn)
+            {
+                conn.Open();
+
+                List<string> allDepartments = new List<string>();
+
+                MySqlDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read() == true)
+                {
+                    string currentDepartment = reader.GetString("Name");
+                    allDepartments.Add(currentDepartment);
+                }
+
+                return allDepartments;
+            }
+        }
+
+        public void DeleteDepartment(string departmentName)
+        {
+            MySqlConnection conn = new MySqlConnection(connectionString);
+
+            MySqlCommand cmd = conn.CreateCommand();
+
+            // parameterized query to prevent SQL Injection
+            cmd.CommandText = "DELETE FROM departments WHERE Name = @departmentName;";
+            cmd.Parameters.AddWithValue("departmentName", departmentName);
+
+            using (conn)
+            {
+                conn.Open();
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        public void UpdateDepartmentName(string currentDepartmentName, string newDepartmentName)
+        {
+            MySqlConnection conn = new MySqlConnection(connectionString);
+
+            MySqlCommand cmd = conn.CreateCommand();
+
+            // parameterized query to prevent SQL Injection
+            cmd.CommandText = "UPDATE departments SET Name = @newName WHERE Name = @currentName;";
+            cmd.Parameters.AddWithValue("newName", newDepartmentName);
+            cmd.Parameters.AddWithValue("currentName", currentDepartmentName);
+
+            using (conn)
+            {
+                conn.Open();
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        public void InsertNewDepartment(string newDepartmentName)
+        {
+            MySqlConnection conn = new MySqlConnection(connectionString);
+
+            MySqlCommand cmd = conn.CreateCommand();
+
+            // parameterized query to prevent SQL Injection
+            cmd.CommandText = "INSERT INTO departments (Name) VALUES (@deptName);";
+            cmd.Parameters.AddWithValue("deptName", newDepartmentName);
+
+            using (conn)
+            {
+                conn.Open();
+                cmd.ExecuteNonQuery();
+            }
+        }
+    }
+}
--- Mark8InClassExamples/Program.cs
+++ Mark8InClassExamples/Program.cs
@@ -1,16 +1,30 @@
 ﻿using System;
-using MySql.Data.MySqlClient;
 using System.Collections.Generic;
 
 namespace Mark8InClassExamples
 {
     class Program
     {
+        static string GetAnswer(string question)
+        {
+            Console.WriteLine(question);
+            return Console.ReadLine();
+        }
+
+        static void DisplayConfirmationMessage(string message)
+        {
+            Console.WriteLine(message);
+            Console.WriteLine("[Press Enter to Continue]");
+            Console.ReadLine();
+        }
+
         static void Main(string[] args)
         {
+            DepartmentRepo repo = new DepartmentRepo();
             string menuChoice = "";
-            while (menuChoice != "QUIT")
+            while (menuChoice.ToUpper() != "QUIT")
             {
+                Console.Clear();
                 Console.WriteLine("What would you like to do?");
                 Console.WriteLine("INSERT");
                 Console.WriteLine("UPDATE");
@@ -19,117 +33,36 @@ namespace Mark8InClassExamples
                 Console.WriteLine("QUIT");
 
                 menuChoice = Console.ReadLine();
+                Console.Clear();
 
                 if (menuChoice.ToUpper() == "INSERT")
                 {
-                    Console.WriteLine("What is the name of the new department?");
-                    string newDepartment = Console.ReadLine();
-                    InsertNewDepartment(newDepartment);
+                    string newDepartment = GetAnswer("What is the name of the new department?");
+                    repo.InsertNewDepartment(newDepartment);
+                    DisplayConfirmationMessage("Department inserted.");
                 }
                 else if (menuChoice.ToUpper() == "UPDATE")
                 {
-                    Console.WriteLine("What deparment name would you like to change?");
-                    string currentName = Console.ReadLine();
-                    Console.WriteLine("What would you like to use as its new name?");
-                    string newName = Console.ReadLine();
-                    UpdateDepartmentName(currentName, newName);
+                    string currentName = GetAnswer("What department name would you like to change?");
+                    string newName = GetAnswer("What would you like to use as its new name?");
+                    repo.UpdateDepartmentName(currentName, newName);
+                    DisplayConfirmationMessage("Department name updated.");
                 }
                 else if (menuChoice.ToUpper() == "DELETE")
                 {
-                    Console.WriteLine("What department name would you like to delete?");
-                    string departmentName = Console.ReadLine();
-                    DeleteDepartment(departmentName);
+                    string departmentName = GetAnswer("What department name would you like to delete?");
+                    repo.DeleteDepartment(departmentName);
+                    DisplayConfirmationMessage("Department deleted.");
                 }
                 else if (menuChoice.ToUpper() == "SELECT")
                 {
-                    List<string> departments = GetAllDepartments();
+                    List<string> departments = repo.GetAllDepartments();
                     foreach (string department in departments)
                     {
                         Console.WriteLine(department);
                     }
+                    Console.ReadLine();
                 }
-            }
-        }
-
-        static List<string> GetAllDepartments()
-        {
-            MySqlConnection conn = new MySqlConnection();
-            conn.ConnectionString = System.IO.File.ReadAllText("ConnectionString.txt");
-
-            MySqlCommand cmd = conn.CreateCommand();
-
-            cmd.CommandText = "SELECT Name FROM departments;";
-
-            using (conn)
-            {
-                conn.Open();
-
-                List<string> allDepartments = new List<string>();
-
-                MySqlDataReader reader = cmd.ExecuteReader();
-
-                while (reader.Read() == true)
-                {
-                    string currentDepartment = reader.GetString("Name");
-                    allDepartments.Add(currentDepartment);
-                }
-
-                return allDepartments;
-            }
-        }
-
-        static void DeleteDepartment(string departmentName)
-        {
-            MySqlConnection conn = new MySqlConnection();
-            conn.ConnectionString = System.IO.File.ReadAllText("ConnectionString.txt");
-
-            MySqlCommand cmd = conn.CreateCommand();
-
-            // parameterized query to prevent SQL Injection
-            cmd.CommandText = "DELETE FROM departments WHERE Name = @departmentName;";
-            cmd.Parameters.AddWithValue("departmentName", departmentName);
-
-            using (conn)
-            {
-                conn.Open();
-                cmd.ExecuteNonQuery();
-            }
-        }
-
-        static void UpdateDepartmentName(string currentDepartmentName, string newDepartmentName)
-        {
-            MySqlConnection conn = new MySqlConnection();
-            conn.ConnectionString = System.IO.File.ReadAllText("ConnectionString.txt");
-
-            MySqlCommand cmd = conn.CreateCommand();
-
-            // parameterized query to prevent SQL Injection
-            cmd.CommandText = "UPDATE departments SET Name = @newName WHERE Name = @currentName;";
-            cmd.Parameters.AddWithValue("newName", newDepartmentName);
-            cmd.Parameters.AddWithValue("currentName", currentDepartmentName);
-
-            using (conn)
-            {
-                conn.Open();
-                cmd.ExecuteNonQuery();
-            }
-        }
-
-        static void InsertNewDepartment(string newDepartmentName)
-        {
-            MySqlConnection conn = new MySqlConnection();
-            conn.ConnectionString = System.IO.File.ReadAllText("ConnectionString.txt");
-
-            MySqlCommand cmd = conn.CreateCommand();
-
-            // parameterized query to prevent SQL Injection
-            cmd.CommandText = "INSERT INTO departments (Name) VALUES (@deptName);";
-            cmd.Parameters.AddWithValue("deptName", newDepartmentName);
-
-            using (conn)
-            {
-                conn.Open();
-                cmd.ExecuteNonQuery();
             }
         }
     }
